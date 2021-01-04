ae39ecf1867dc3821e276756d5e88026ae9897ec
MushroomCat0815 <69503274+MushroomCat0815@users.noreply.github.com>
2021-01-04 22:13:30 +0800

    Add files via upload

--- server_example/js/socket.js
+++ server_example/js/socket.js
@@ -1,10 +1,38 @@
 $(document).ready(function () {
-	var url = 'http://192.168.1.124';
+	var url = 'http://192.168.1.182';
 	var port = '5001';
 	var socket = io.connect(url + ':' + port);
 	socket.on('connect', function () {
 		socket.emit('connect_event', { data: 'test connected!' });
+
+		socket.on('connect', function(){
+			$('#control_up').click(function(msg) {
+			socket.emit('button_press',{data : 'w'});
+			});
+		});
+		socket.on('connect', function(){
+			$('#control_down').click(function(msg) {
+			socket.emit('button_press',{data : 's'});
+			});
+		});
+		socket.on('connect', function(){
+			$('#control_left').click(function(msg) {
+			socket.emit('button_press',{data : 'a'});
+			});
+		});
+		socket.on('connect', function(){
+			$('#control_right').click(function(msg) {
+			socket.emit('button_press',{data : 'd'});
+			});
+		});
+		socket.on('connect', function(){
+			$('#control_enter').click(function(msg) {
+			socket.emit('button_press',{data : 'o'});
+			});
+		});
+
 	});
+
 	socket.on('server_response', function (msg) {
 		$('#log').append(
 			'<br>' +
