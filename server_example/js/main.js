6e3bf59d6c669957ff94d4785d44df5ec96288e4
BK <bk@aliens.tw>
2021-01-04 22:29:08 +0800

    22323

--- server_example/js/main.js
+++ server_example/js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-	var profile = googleUser.getBasicProfile();
 	console.log('tttt');
 	$('#textboard_submit').on('click', function () {
 		console.log($('#textboard_text').val());
@@ -12,9 +11,6 @@ $(document).ready(function () {
 		console.log(123)
 		;
 	});
-
-	
-	console.log(profile, "profile");
 
 	
 });
@@ -47,3 +43,18 @@ $(document).on('keypress',function(e) {
 		controlBtnActive($("#control_enter"))
 	}
 });
+
+function onSignIn(googleUser) {
+	// Useful data for your client-side scripts:
+	var profile = googleUser.getBasicProfile();
+	console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+	console.log('Full Name: ' + profile.getName());
+	console.log('Given Name: ' + profile.getGivenName());
+	console.log('Family Name: ' + profile.getFamilyName());
+	console.log("Image URL: " + profile.getImageUrl());
+	console.log("Email: " + profile.getEmail());
+
+	// The ID token you need to pass to your backend:
+	var id_token = googleUser.getAuthResponse().id_token;
+	console.log("ID Token: " + id_token);
+  }
