d240d0d2195a2547e6a52a215054e3e2f338bf65
MushroomCat0815 <69503274+MushroomCat0815@users.noreply.github.com>
2021-01-05 21:09:34 +0800

    Add files via upload

--- server_example/js/main.js
+++ server_example/js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
-	console.log('tttt');
+	console.log('test');
+	$('#textboard_submit').on('click', function () {
+		console.log($('#textboard_text').val());
+		$('.board-show-wrap').append(
+			"<div class='show-item-wrap'><div class='show-item'><div class='item-user'>a</div><div class='context'>"+ $('#textboard_text').val() +"</div></div></div>"
+		);
+		$('#textboard_text').val("");
+		;
+	});
+
+	
 });
 $(document).on('keypress',function(e) {
 	function controlBtnActive(target){
@@ -30,3 +40,23 @@ $(document).on('keypress',function(e) {
 		controlBtnActive($("#control_enter"))
 	}
 });
+function onSignIn(googleUser) {
+	// Useful data for your client-side scripts:
+	var profile = googleUser.getBasicProfile();
+	console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+	console.log('Full Name: ' + profile.getName());
+	console.log('Given Name: ' + profile.getGivenName());
+	console.log('Family Name: ' + profile.getFamilyName());
+	console.log("Image URL: " + profile.getImageUrl());
+	console.log("Email: " + profile.getEmail());
+
+	// The ID token you need to pass to your backend:
+	var id_token = googleUser.getAuthResponse().id_token;
+	console.log("ID Token: " + id_token);
+  }
+  function signOut() {
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function () {
+	  console.log('User signed out.');
+	});
+  }
