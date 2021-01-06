0960a38e985a35e44f3df4666080f93fcffa96a2
MushroomCat0815 <69503274+MushroomCat0815@users.noreply.github.com>
2021-01-06 15:10:57 +0800

    Add files via upload

--- server_example/js/main.js
+++ server_example/js/main.js
@@ -1,16 +1,4 @@
-$(document).ready(function () {
-	console.log('test');
-	$('#textboard_submit').on('click', function () {
-		console.log($('#textboard_text').val());
-		$('.board-show-wrap').append(
-			"<div class='show-item-wrap'><div class='show-item'><div class='item-user'>a</div><div class='context'>"+ $('#textboard_text').val() +"</div></div></div>"
-		);
-		$('#textboard_text').val("");
-		;
-	});
 
-	
-});
 $(document).on('keypress',function(e) {
 	function controlBtnActive(target){
 		$(".move-wrap").find("button").removeClass("active");
